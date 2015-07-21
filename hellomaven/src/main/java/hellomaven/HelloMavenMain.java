cdbaaef415282aef8f4b9fb00077baedcb155521
antoalmobeni <antoalmobeni@msn.com>
2015-07-21 12:00:51 +0100

    Second commit!

--- hellomaven/src/main/java/hellomaven/HelloMavenMain.java
+++ hellomaven/src/main/java/hellomaven/HelloMavenMain.java
@@ -1,14 +1,21 @@
 package hellomaven;
 
+
+import java.io.IOException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class HelloMavenMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		Logger logger = LoggerFactory.getLogger(HelloMavenMain.class);
 		logger.info("Hello Maven");
+		
+		int i = System.in.read();
+		System.out.println("Number got is: " + i);
+	
 	}
 
 }
